82e19896e89bec9b3a2c7c355fb5f5e237a56ded
Marcintt <marcin.wikarski@ttpsc.pl>
2019-07-26 08:58:12 +0200

    Create users in Jira

---
+++ src/test/java/createteamtest/AddHRGroup.java
@@ -0,0 +1,37 @@
+package createteamtest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AddHRGroup {
+
+    @Test
+    public void AddHRGroup() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(Config.ADDRESS);
+
+        LoginFunction.login(driver, Config.ADMIN_LOGIN, Config.ADMIN_PASSWORD);
+
+        WebElement adminMenuButton = driver.findElement(By.xpath("//a[contains(@id, 'admin_menu')]"));
+        adminMenuButton.click();
+        WebElement userManagementButton = driver.findElement(By.xpath("//a[contains(@id, 'admin_users_menu')]"));
+        userManagementButton.click();
+        WebElement adminAccessField = driver.findElement(By.xpath("//input[contains(@id, 'login-form-authenticatePassword')]"));
+        adminAccessField.sendKeys("admin");
+        WebElement confirmButton = driver.findElement(By.xpath("//input[contains(@id, 'login-form-submit')]"));
+        confirmButton.click();
+        WebElement groupsButton = driver.findElement(By.xpath("//a[contains(@id, 'group_browser')]"));
+        groupsButton.click();
+        WebElement groupNameField = driver.findElement(By.name("addName"));
+        groupNameField.sendKeys("HR group");
+        WebElement addGroupButton = driver.findElement(By.name("add_group"));
+        addGroupButton.click();
+        WebElement editMembersButton = driver.findElement(By.xpath("//a[contains(@id, 'edit_members_of_HR group')]"));
+        editMembersButton.click();
+    }
+}
